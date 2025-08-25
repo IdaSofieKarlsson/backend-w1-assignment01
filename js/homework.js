7e55c7d35ca8de316d9a6bf50b43a07aa99c70ac
Ida-Sofie Karlsson <ida.sofie.karlsson@gmail.com>
2025-08-25 13:35:28 +0200

    first exercise

---
+++ js/homework.js
@@ -0,0 +1,8 @@
+//const, because my name doesn't change
+const firstName = 'Ida-Sofie';
+
+//let, because this may change
+let favoriteNumber = 13;
+let likesCoding = true;
+
+console.log(`Hi, my name is ${firstName}, my favorite number is ${favoriteNumber}, and it is ${likesCoding} that I like coding.`);
